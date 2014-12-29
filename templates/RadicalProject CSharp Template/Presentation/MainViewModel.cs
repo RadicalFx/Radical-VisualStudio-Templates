00033ecd81f3a1e02e46ead452e86111494e059d
Mauro Servienti <mauro@topics.it>
2014-12-29 07:44:26 +0100

    removing the pre-setup code, it is noise in the long term

--- templates/RadicalProject CSharp Template/Presentation/MainViewModel.cs
+++ templates/RadicalProject CSharp Template/Presentation/MainViewModel.cs
@@ -5,15 +5,6 @@ namespace RadicalTemplate.Presentation
 {
 	class MainViewModel : AbstractViewModel
 	{
-		public MainViewModel()
-		{
-			this.SetInitialPropertyValue( () => this.SampleText, "Welcome to Radical." );
-		}
-
-		public String SampleText
-		{
-			get { return this.GetPropertyValue( () => this.SampleText ); }
-			set { this.SetPropertyValue( () => this.SampleText, value ); }
-		}
+		
 	}
 }
